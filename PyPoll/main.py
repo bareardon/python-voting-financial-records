1b5693b0681d68970f7a77a4a8e622f50b8aaa01
bareardon <73446403+bareardon@users.noreply.github.com>
2020-12-03 15:03:33 -0500

    final
    final product

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -41,28 +41,31 @@ with open(election_csv_path) as csvfile:
                 candidate_votes[name] += 1 
             else:
                 candidate_votes[name] += 1
+
+print(candidate_votes)
+
     
-# Calculate the percentage of votes each candidate won
-# Create a new dictionary with candidate name and votes with same values from candidate_votes
+# Calculate the percentage of votes each candidate won and create a new dictionary with candidate name and votes with same values from candidate_votes
 candidate_votes_percentage = dict(candidate_votes)  
 
-for name in candidate_votes_percentage:
-    candidate_votes_percentage[name] = f"{round(((candidate_votes_percentage[name] / (total_votes))*100), 3)}%"
+vote_res = ""
+for key, value in candidate_votes_percentage.items():
+    vote_res += key + ' '+ str(round(((value/total_votes) *100), 3))+ '%' + ' ' + '(' + str(value) + ')' + '\n'
 
-for i in (candidate_votes_percentage, candidate_votes):
-    for key,value in i.items():
-        combined_votes_percentages[key].append(value)
+print(vote_res)    
 
 # Determine the winner of the election based on the poplar vote 
 winner_name = max(candidate_votes.items(), key=operator.itemgetter(1))[0]
+
+print(winner_name)
 
 # Set format for printing 
 output = (
     f"Election Results\n"
     f"-----------------\n"
-    f"Total Votes: {total_votes}\n"
+    f"Total Votes: {total_votes} \n"
     f"-----------------\n"
-    f"{combined_votes_percentage}\n"
+    f"{vote_res} \n"
     f"-----------------\n"
     f"Winner: {winner_name}\n"
     f"-----------------"
