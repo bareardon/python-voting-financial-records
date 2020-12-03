d7672dd07dbe5bb64c21f1255aab7c75e17e3036
bareardon <73446403+bareardon@users.noreply.github.com>
2020-12-02 20:31:42 -0500

    Percentage
    Percentage commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,22 @@
 import os
 import csv
+import operator 
+from collections import defaultdict
 
 # Path to collect data from Resources folder
-election_csv_path = os.path.join('Recources', 'election_data.csv')
+election_csv_path = os.path.join('Resources', 'election_data.csv')
 
 # Define Variables 
 total_votes = 0 
+candidate_votes = {}
+candidate_votes_percentage = {}
+candidates = []
+percentage = 0 
+winner_name = []
 
-winner = {'candidate': 2}
 
 # Read is csv file
-with open(election_csv_path) as csvfile
+with open(election_csv_path) as csvfile:
 
     # Split the data on commas 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -23,28 +29,49 @@ with open(election_csv_path) as csvfile
         # Calculate the total number of votes cast 
         total_votes = total_votes + 1
 
-        # Generate a list of candidates who recevied votes
+        name = row[2]
 
-        # Calculate the percentage of votes each candidate won
-        total_votes = 
-        
-        # Calculate the total number of votes each candidate won
-        if 
+        # Calculate the total number of votes each candidate won- 
+        if name not in candidate_votes:
+            candidate_votes[name] = 1
+        else:
+            if candidate_votes[name] == 1:
+                candidates.append(name)
+                candidate_votes[name] += 1 
+            else:
+                candidate_votes[name] += 1
+    
+# Calculate the percentage of votes each candidate won
 
-        # Determine the winner of the election based on the poplar
-        # use max function to see who received most votes 
+# Create a new dictionary with candidate name and votes with same values from candidate_votes
+candidate_votes_percentage = dict(candidate_votes)  
 
-# Set format for printing 
+for name in candidate_votes_percentage:
+    candidate_votes_percentage[name] = round(((candidate_votes_percentage[name] / (total_votes))*100), 2)
+
+print(candidate_votes_percentage)
+
+for name in candidate_votes_percentage:
+    candidate_votes_percentage[value].append(name)  
+      
+    
+#print(candidates)
+print(candidate_votes)
+
+# Determine the winner of the election based on the poplar vote 
+winner_name = max(candidate_votes.items(), key=operator.itemgetter(1))[0]
+
+print(winner_name)
+
+# # Set format for printing 
 output = (
     f"Election Results\n"
     f"-----------------\n"
-    f"Total Votes {}\n"
+    f"Total Votes: {total_votes}\n"
     f"-----------------\n"
-
-     f"-----------------\n"
-     f"Winner {}\n"
-
-
+    f"{candidate_votes}\n"
+    f"-----------------\n"
+    f"Winner: {winner_name}"
 )
 
 with open("analysis/output.txt", "w") as txt_file: 
